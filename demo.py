667e69318a84df1eee0af37ab9dcfcc2007585f7
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2025-08-24 17:39:02 +0600

    logger, exception & utilis added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    r = 3/0
+    print(r)
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys)
